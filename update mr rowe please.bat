726ad679dde77d8b58440a1bf075d61391c9e041
Gage Walker <GWalker7046@student.pps.net>
2018-05-16 15:42:42 -0700

    added many files

---
+++ update mr rowe please.bat
@@ -0,0 +1,12 @@
+@echo off
+echo.
+echo copying giles
+echo.
+xcopy .  y:\ddccode2  /s /y
+
+xcopy finalproject  y:\ddccode1\assignment4\finalproject
+
+echo.
+echo done copying, mr. rowe says thanks
+echo.
+pause 
